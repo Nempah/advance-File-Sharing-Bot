76215e3259297268792c57f2fa1c9df08dc6b4f2
Nempah <109466880+Nempah@users.noreply.github.com>
2022-08-08 13:33:24 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/GreyMatter658/advance-File-Sharing-Bot'>Click here</a>\n○ Channel : @GreyMatter_bots\n○ Support Group : @GreyMatters_bots_discussion</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={https://www.instagram.com/denathyon/}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @IrisMovies\n○ Menu : @IrisCollectionBot</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
